fb32447fd8b45aee8cac049bf820672d12c75da7
chanwookpark <chanwook.god@gmail.com>
2017-10-30 02:04:31 +0900

    8장 실습 끗

--- ch08/webpack-start/webpack.config.js
+++ ch08/webpack-start/webpack.config.js
@@ -3,5 +3,26 @@ module.exports = {
     entry: {
         main: __dirname + '/src/index.js'
     },
-    output:
+    output: {
+        path: __dirname + '/public/dist/',
+        filename: '[name].js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    devServer: {
+        contentBase: './public/',
+        port: 3000,
+        historyApiFallback: true
+    }
 }
